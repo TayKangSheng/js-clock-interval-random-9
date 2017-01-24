8258dd040b5b7713b7ef36b2d445f6d50731a370
xavier poh <xvrpke@gmail.com>
2017-01-24 18:55:52 +0800

    complete clock.js part 1

---
+++ clock.js
@@ -0,0 +1,29 @@
+var count = 0
+
+function secondInterval () {
+  // count++
+  // if (count === 60) {
+  //   count = 0
+  // }
+  return count++
+}
+setInterval(secondInterval, 1000)
+
+setInterval(function secondRotation () {
+  var degrees = ((count % 60) * 6)
+  console.log(degrees)
+  var secondHand = document.getElementById('second')
+  secondHand.style.transform = 'rotate(' + degrees + 'deg)'
+}, 1000)
+
+setInterval(function minuteRotation () {
+  var degrees = ((count / 3600) * 360) % 360
+  var minuteHand = document.getElementById('minute')
+  minuteHand.style.transform = 'rotate(' + degrees + 'deg)'
+}, 1000)
+
+setInterval(function hourRotation () {
+  var degrees = ((count / 43200) * 360) % 360
+  var hourHand = document.getElementById('hour')
+  hourHand.style.transform = 'rotate(' + degrees + 'deg)'
+}, 1000)
